93a9e98d9a5c516162771c68cc0a37b738209202
nembo93 <francesco.c193@libero.it>
2023-01-09 17:28:14 +0100

    Aggiunge costruttore a classe Movie, passa a oggetti istanziati argomento anno

--- index.php
+++ index.php
@@ -3,10 +3,20 @@
 class Movie{
     public $nome;
     public $anno;
+
+    function __construct($_anno) {
+        $this->anno = $_anno;
+    }
 }
 
-$inception = new Movie();
+$inception = new Movie(2006);
+$inception->nome = "Inception";
+// $inception->anno = "2006";
 
-$blood_diamond = new Movie ();
 
-var_dump($blood_diamond);
+$blood_diamond = new Movie (2000);
+$blood_diamond->nome = "Blood diamond";
+// $blood_diamond->anno = "2000";
+
+
+var_dump($inception);
