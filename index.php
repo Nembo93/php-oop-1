1619cef900c86e687400c7542c126aa67f71bb8e
nembo93 <francesco.c193@libero.it>
2023-01-09 17:58:27 +0100

    Aggiunge commenti

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
-
+// Definizione della classe dei film
 class Movie{
+    // Definizione degli attributi
     public $nome;
     public $anno;
     public $top_rated = null;
 
+    // Definizione del costruttore (in fase di creazione dell'istanza/oggetto gli viene passato il valore dell'anno di uscita)
     function __construct($_anno) {
         $this->anno = $_anno;
     }
 
+    // Definizione di un metodo (la funzione accetta come argomento il valore del voto che a seguito di un controllo stabilisce se il film è tra i più votati o no)
     public function setVoto($voto){
         if ($voto > 4){
             $this->top_rated = true;
@@ -22,11 +25,12 @@ class Movie{
 }
 
 
-
+// Istanza/oggetto film Inception
 $inception = new Movie(2006);
 $inception->nome = "Inception";
 $inception->setVoto(5);
 
+// Istanza/oggetto film Blood diamond
 $blood_diamond = new Movie (2000);
 $blood_diamond->nome = "Blood diamond";
 $blood_diamond->setVoto(4);
