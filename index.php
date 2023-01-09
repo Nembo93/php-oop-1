b3869a6b6cabe3879317645050058f5ec015d962
nembo93 <francesco.c193@libero.it>
2023-01-09 17:44:08 +0100

    Aggiunge metodo a classe Movie per stabilire se il film è tra i più votati

--- index.php
+++ index.php
@@ -3,20 +3,34 @@
 class Movie{
     public $nome;
     public $anno;
+    public $top_rated = null;
 
     function __construct($_anno) {
         $this->anno = $_anno;
     }
+
+    public function setVoto($voto){
+        if ($voto > 4){
+            $this->top_rated = true;
+        } else {
+            $this->top_rated = false;
+        }
+    }
+    public function getVoto(){
+        return $this->top_rated;
+    }
 }
+
+
 
 $inception = new Movie(2006);
 $inception->nome = "Inception";
-// $inception->anno = "2006";
-
+$inception->setVoto(5);
 
 $blood_diamond = new Movie (2000);
 $blood_diamond->nome = "Blood diamond";
-// $blood_diamond->anno = "2000";
+$blood_diamond->setVoto(4);
 
 
 var_dump($inception);
+var_dump($blood_diamond);
